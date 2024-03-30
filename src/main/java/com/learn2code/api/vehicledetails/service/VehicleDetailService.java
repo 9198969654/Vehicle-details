58ac4f3ca18c54c95ae90c7aa83a22b985154c77
User <narendrakumarnishad56@gmail.com>
2024-03-30 13:21:41 +0530

    Delete Security part

--- src/main/java/com/learn2code/api/vehicledetails/service/VehicleDetailService.java
+++ src/main/java/com/learn2code/api/vehicledetails/service/VehicleDetailService.java
@@ -4,5 +4,7 @@ import com.learn2code.api.vehicledetails.enteties.VehicleDetail;
 import com.learn2code.api.vehicledetails.errors.VehicleNotSaved;
 
 public interface VehicleDetailService {
+
+
     VehicleDetail saveVehicleDetails(VehicleDetail vehicleDetail) throws VehicleNotSaved;
 }
